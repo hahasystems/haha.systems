ff2ee6a97d361404a310f10bd58e34acbb7433d2
Mark Anthony Gibbins <xiy3x0@gmail.com>
2023-11-30 15:47:37 +0000

    chore: fix ci

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,13 +3,20 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `haha.systems`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://haha.systems`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-google-gtag", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
+  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-image", "gatsby-plugin-sitemap",
+  {
+    resolve: 'gatsby-plugin-google-gtag',
+    options: {
+      "trackingIds": ["G-0ZQZQZQZQZ"]
+    }
+  },
+  {
     resolve: 'gatsby-plugin-manifest',
     options: {
       "icon": "src/images/icon.png"
